1d10a4deda3f9b6f62696a44ca8c3201fece1330
Emil Nuutinen <emil@emilnuutinen.com>
2021-12-25 16:46:46 +0200

    Day 1, part 2 solved

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -1,18 +1,38 @@
+use std::cmp;
 use std::fs;
 
 fn main() {
     let data = fs::read_to_string("input.txt").expect("File not found");
     let mut prev_val = 1000;
-    let mut result = 0;
+    let mut result_a = 0;
+
+    let numbers: Vec<u16> = data
+        .lines()
+        .map(|line| line.parse::<u16>().unwrap())
+        .collect();
 
     for line in data.lines() {
         let val: u16 = line.parse::<u16>().unwrap();
 
         if prev_val < val {
-            result += 1;
+            result_a += 1;
         }
         prev_val = val;
     }
 
-    println!("{}", result);
+    println!("{}", result_a);
+
+    let mut prev_sum = 1000;
+    let mut result_b = 0;
+
+    for index in 0..numbers.len() {
+        let ref slice = &numbers[index..cmp::min(index + 3, numbers.len())];
+        let sum: u16 = slice.iter().sum();
+
+        if prev_sum < sum {
+            result_b += 1;
+        }
+        prev_sum = sum;
+    }
+    println!("{}", result_b);
 }
